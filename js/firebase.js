3cbcd737c05b27daded7e3de722798b15b8c9a62
malsadi87 <33456519+malsadi87@users.noreply.github.com>
2020-09-21 04:08:10 +0100

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,18 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAJk97R5-4ICyRw3og1vXiimvyRelhVdfo",
+    authDomain: "startup-be728.firebaseapp.com",
+    databaseURL: "https://startup-be728.firebaseio.com",
+    projectId: "startup-be728",
+    storageBucket: "startup-be728.appspot.com",
+    messagingSenderId: "537350616196",
+    appId: "1:537350616196:web:c82d171a50639fe6557ed1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+
+
+  //db.settings({timestampsInSnapshots : true});
+
